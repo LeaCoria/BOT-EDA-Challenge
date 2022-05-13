918b760977a1842dca95dd059bdfd3b808242ea5
LeaCoria <leandrocoria19@gmail.com>
2022-05-12 23:24:51 -0300

    Agrego funcion de aceptar challenge e intento implementar la conexion a clase movimiento

--- connection.py
+++ connection.py
@@ -1,22 +1,21 @@
-from importlib.metadata import SelectableGroups
-import re
-import websocket
+import websockets
 import time
 import json
 import asyncio
+import makemoves
 
 class Connection():
     def __init__(self,token):
         
-            self.uri = f"wss://4yyity02md.execute-api.us-east-1.amazonaws.com/ws?token=~{token}"
+            self.uri = f"wss://4yyity02md.execute-api.us-east-1.amazonaws.com/ws?token={token}"
             self.user_list = list()
             
     async def start(self):
         
         while True:
             try:
-                print(f"Connection to {self.uri}")
-                async with websocket.connect(self.uri) as websocket:
+                #print(f"\n\nConnection to \n\n{self.uri}\n\n")
+                async with websockets.connect(self.uri) as websocket:
                     await self.play(websocket)
             except Exception:
                 print("Connection error!")
@@ -29,10 +28,32 @@ class Connection():
                 request = await websocket.recv()
                 print(f"{request}")
                 requestData = json.loads(request)
-                if requestData['event'] == 'update_user_list':
-                    self.user_list = requestData['data']['users']
-                    print(f"Updated user_list: {self.user_list}")
+                if requestData["event"] == "update_user_list":
+                    self.user_list = requestData["data"]["users"]
+                    print(f"\n\nUpdated user_list: \n{self.user_list}")
+                if requestData["event"] == "challenge":
+                    if requestData["data"]["opponent"] == "leandrocoria19@gmail.com":
+                        await self.send(
+                                websocket,
+                                "accept_challenge",
+                                {
+                                    "challenge_id": requestData["data"]["challenge_id"],
+                                }
+                                )
+                if requestData["event"] == "your_turn":
+                    makeMoves = makemoves.Makemoves(requestData)
+                    await makeMoves.mover()
             except Exception:
                 print(f"Error {Exception}")
                 break
-                
+    
+    async def send (self,websocket,action,data):
+        
+        message = json.dumps(
+            {
+                'action' : action,
+                'data': data,
+            }
+        )
+        print(message)
+        await websocket.send(message)
