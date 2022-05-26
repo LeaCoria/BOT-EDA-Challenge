dfd29b26ab6f2a2b48960c65d9cfa5fbb6c50f8f
LeaCoria <leandrocoria19@gmail.com>
2022-05-26 17:36:19 -0300

    Agregado de saltos diagonales

--- horizontal_move.py
+++ horizontal_move.py
@@ -24,10 +24,26 @@ class Horizontal_Move():
     
     def valid_horizontal_jump(self, myPeons, enPeons, jumpPeonsHorizontal, freePeonsHorizontal, wallsH, wallsV):
         
+        if self.requestData["data"]["side"] == "N":
+            sideIsNorth = True
+        else:
+            sideIsNorth = False
+        
         jumpPeonsHorizontalValid = []
+        jumpPeonsDiagonal = []
         for peon in jumpPeonsHorizontal:
-            if not( [ peon[0] - 1 , peon[1] - 1 ] in wallsV ) and not( [ peon[0] - 1 , peon[1] ] in wallsV ):
-                jumpPeonsHorizontalValid.append(peon)
+            if sideIsNorth:
+                if not( [ peon[0] - 1 , peon[1] - 1 ] in wallsV ) and not( [ peon[0] , peon[1] - 1 ] in wallsV ):
+                    if not( [ peon[0] -1 , peon[1] -2 ] in wallsV ) and not( [ peon[0] , peon[1] -2 ] in wallsV ):
+                        jumpPeonsHorizontalValid.append(peon)
+                    else:
+                        jumpPeonsDiagonal.append(peon)
+            else:
+                if not( [ peon[0] , peon[1] -1 ] in wallsV ) and not( [ peon[0] - 1, peon[1] - 1 ] in wallsV ):
+                    if not( [ peon[0] , peon[1] - 2 ] in wallsV ) and not( [ peon[0] - 1 , peon[1] - 2 ] in wallsV ):
+                        jumpPeonsHorizontalValid.append(peon)
+                    else:
+                        jumpPeonsDiagonal.append(peon)
         if len(jumpPeonsHorizontalValid) > 0:
             for peon in jumpPeonsHorizontalValid:
                 to_row = peon[0]
@@ -35,12 +51,68 @@ class Horizontal_Move():
                 if ( [ to_row , to_col ] in enPeons ) or ( [ to_row , to_col ] in myPeons ):
                     jumpPeonsHorizontalValid.remove(peon)
         else:
-            return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
+            if len(jumpPeonsDiagonal) > 0:
+                return self.valid_diagonal_jump(myPeons, enPeons, jumpPeonsDiagonal, freePeonsHorizontal, wallsH, wallsV)
+            else:
+                return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
         if len(jumpPeonsHorizontalValid) > 0:
             return self.move_peon_jump(jumpPeonsHorizontalValid)
         else:
-            return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
+            if len(jumpPeonsDiagonal) > 0:
+                return self.valid_diagonal_jump(myPeons, enPeons, jumpPeonsDiagonal, freePeonsHorizontal, wallsH, wallsV)
+            else:
+                return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
+    
+    def valid_diagonal_jump(self, myPeons, enPeons, jumpPeonsDiagonal, freePeonsHorizontal, wallsH, wallsV):
         
+        if self.requestData["data"]["side"] == "N":
+            sideIsNorth = True
+        else:
+            sideIsNorth = False
+        
+        jumpPeonsDiagonalUpValid = []
+        jumpPeonsDiagonalDownValid = []
+        for peon in jumpPeonsDiagonal:
+            if sideIsNorth:
+                if not( [ peon[0] , peon[1] - 1 ] in wallsH ) and not( [ peon[0] , peon[1] - 2 ] in wallsH ):
+                    jumpPeonsDiagonalUpValid.append(peon)
+                if not( [ peon[0] - 1, peon[1] - 1 ] in wallsH ) and not( [ peon[0] - 1 , peon[1] - 2 ] in wallsH ):
+                    jumpPeonsDiagonalDownValid.append(peon)
+            else:
+                if not( [ peon[0] - 1 , peon[1] - 1 ] in wallsH ) and not( [ peon[0] - 1 , peon[1] - 2 ] in wallsH ):
+                    jumpPeonsDiagonalUpValid.append(peon)
+                if not( [ peon[0] , peon[1] - 1 ] in wallsH ) and not( [ peon[0] , peon[1] - 2 ] in wallsH ):
+                    jumpPeonsDiagonalDownValid.append(peon)
+        if len(jumpPeonsDiagonalUpValid) > 0:
+            for peon in jumpPeonsDiagonalUpValid:
+                if sideIsNorth:
+                    if ( [ peon[0] + 1 , peon[1] - 1 ] in myPeons ) or ( [ peon[0] + 1 , peon[1] - 1 ] in enPeons ):
+                        jumpPeonsDiagonalUpValid.remove(peon)
+                        if not( peon in jumpPeonsDiagonalDownValid ):
+                            jumpPeonsDiagonalDownValid.append(peon)
+                else:
+                    if ( [ peon[0] - 1 , peon[1] - 1 ] in myPeons ) or ( [ peon[0] - 1 , peon[1] - 1 ] in enPeons ):
+                        jumpPeonsDiagonalUpValid.remove(peon)
+                        if not( peon in jumpPeonsDiagonalDownValid ):
+                            jumpPeonsDiagonalDownValid.append(peon)
+        if len(jumpPeonsDiagonalUpValid) > 0:
+            return self.move_peon_jump_diagonal_up(jumpPeonsDiagonalUpValid)
+        else:
+            if len(jumpPeonsDiagonalDownValid) > 0:
+                for peon in jumpPeonsDiagonalDownValid:
+                    if sideIsNorth:    
+                        if ( [ peon[0] - 1 , peon[1]-1 ] in enPeons) or ( [ peon[0] - 1 , peon[1] - 1 ] in myPeons ):
+                            jumpPeonsDiagonalDownValid.remove(peon)
+                    else:
+                        if ( [ peon[0] + 1 , peon[1]-1 ] in enPeons) or ( [ peon[0] + 1 , peon[1] - 1 ] in myPeons ):
+                            jumpPeonsDiagonalDownValid.remove(peon)
+            else:
+                return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
+        if len(jumpPeonsDiagonalDownValid) > 0:
+            return self.move_peon_jump_diagonal_down(jumpPeonsDiagonalDownValid)
+        else:
+            return self.valid_horizontal_move(myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV)
+                    
     def valid_horizontal_move(self, myPeons, enPeons, freePeonsHorizontal, wallsH, wallsV):
         
         freePeonsHorizontalValid = []
@@ -67,6 +139,46 @@ class Horizontal_Move():
                     from_col = peon[1]
                     to_row = from_row
                     to_col = to_col - 2
+        return self.send(from_row, from_col, to_row, to_col)
+    
+    def move_peon_jump_diagonal_up(self, jumpPeonsDiagonalUpValid):
+        
+        if self.requestData["data"]["side"] == "N":
+            sideIsNorth = True
+        else:
+            sideIsNorth = False
+        
+        from_row = jumpPeonsDiagonalUpValid[0][0]
+        from_col = jumpPeonsDiagonalUpValid[0][1]
+        to_row = from_row + ( 1 if sideIsNorth else -1 )
+        to_col = from_col - 1
+        if len(jumpPeonsDiagonalUpValid) > 1:
+            for peon in jumpPeonsDiagonalUpValid:
+                if ( sideIsNorth and from_row < peon[0] ) or (not sideIsNorth and from_row > peon[0] ):
+                    from_row = peon[0]
+                    from_col = peon[1]
+                    to_row = from_row + ( 1 if sideIsNorth else -1 )
+                    to_col = from_col - 1
+        return self.send(from_row, from_col, to_row, to_col)
+
+    def move_peon_jump_diagonal_down(self, jumpPeonsDiagonalDownValid):
+        
+        if self.requestData["data"]["side"] == "N":
+            sideIsNorth = True
+        else:
+            sideIsNorth = False
+        
+        from_row = jumpPeonsDiagonalDownValid[0][0]
+        from_col = jumpPeonsDiagonalDownValid[0][1]
+        to_row = from_row + ( -1 if sideIsNorth else 1 )
+        to_col = from_col - 1
+        if len(jumpPeonsDiagonalDownValid) > 1:
+            for peon in jumpPeonsDiagonalDownValid:
+                if ( sideIsNorth and from_row < peon[0] ) or (not sideIsNorth and from_row > peon[0] ):
+                    from_row = peon[0]
+                    from_col = peon[1]
+                    to_row = from_row + ( -1 if sideIsNorth else 1 )
+                    to_col = from_col - 1
         return self.send(from_row, from_col, to_row, to_col)
     
     def move_peon_free(self, freePeonsHorizontalValid):
